ab89fcdc4359cbba81c8664e7bdbefe82d8cc179
licia-santos <liciasantos028@gmail.com>
2023-03-08 12:53:30 -0300

    Adicionar arquivos de projeto.

---
+++ pessoa/Pessoa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pessoa;
+
+public class Pessoa
+{
+    public int Id;
+    public string Nome;
+    public string Telefone;
+
+    public Pessoa(int id, string nome, string telefone)
+    {
+        Id = id;
+        Nome = nome;
+        Telefone = telefone;
+    }
+
+    public int getid()
+    {
+        return Id;
+    }
+
+    public void setid(int id)
+    {
+        if (id > 0)
+        {
+            Id = id;
+        }
+
+    }
+
+    public string getNome()
+    {
+        return Nome;
+    }
+
+    public void setNome(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string getTelefone()
+    {
+        return Telefone;
+    }
+
+    public void setTelefone(string telefone)
+    {
+        Telefone = telefone;
+    }
+
+}
---
+++ pessoa/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using pessoa;
+
+Console.WriteLine("Hello, World!"); Pessoa p1 = new Pessoa(1, "Lícia", "(16)9999-8888");
+Console.WriteLine(p1.getNome() + " " + p1.getTelefone());
